527e1e496824b53f855c635ff6db16d099f1b2c5
Andrii_Babenko <andrey.brnr@gmail.com>
2020-12-05 14:10:09 +0200

    task-3 DONE

--- js/task-7.js
+++ js/task-7.js
@@ -45,3 +45,4 @@
 // console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
 // console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 // console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
